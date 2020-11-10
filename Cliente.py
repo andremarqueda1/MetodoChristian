00e32cd946b8455217e253fb02db8dae24042d69
andremarqueda <andremarqueda1@gmail.com>
2020-11-10 17:52:28 -0600

    Added final report on the clientSide

--- Cliente.py
+++ Cliente.py
@@ -10,52 +10,54 @@ import datetime
 import time
 import random
 context = zmq.Context()
-
-
 print("Conectandose al proceso S…")
 socket = context.socket(zmq.REQ)
-socket.connect("tcp://localhost:4595")
-print("Solicitando tiempo a S")
-
+socket.connect("tcp://localhost:6865")
+print("Solicitando tiempo a S") 
 msg = "solicitud de tiempo"
 socket.send(msg.encode('utf-8'))
-RTT=time.time() #inicio del cronómetro
-tiempoP=datetime.datetime.now()#Hora de P
-
+RTTstart=time.time() #inicio del cronómetro
+tiempoPold=datetime.datetime.now()#Hora de P
+        
 """
-Restando segundos aleatorios para simular una desincronización
+        Restando segundos aleatorios para simular una desincronización
 """
-
-tiempoP=tiempoP-datetime.timedelta(seconds=random.randint(0,9))
+        
+tiempoP=tiempoPold-datetime.timedelta(seconds=random.randint(0,9))
 print("La hora actual en P previo a la sincronización es:\n",tiempoP.strftime("%H:%M:%S"))
-
-
+        
+        
 """
-Recibiendo respuesta del servidor S
+        Recibiendo respuesta del servidor S
 """
-
-tiempoServidor = socket.recv().decode('utf-8') #Recibiendo tiempo
-
+        
+tiempoServidor = socket.recv().decode('utf-8') 
+        
 """
-Una vez recibido la respuesta de S, podemos calcular el Round Trip Time
+        Una vez recibido la respuesta de S, podemos calcular el Round Trip Time
 """
-
-RTT=time.time()-RTT #fin del cronómetro calculando el Round Trip Time
-print("El tiempo que entre envío de solicitud y recepción de la hora (RTT) fue de:\n\tRTT: ",RTT)
+        
+RTTend=time.time()-RTTstart #fin del cronómetro calculando el Round Trip Time
+print("El tiempo que entre envío de solicitud y recepción de la hora (RTT) fue de:\n\tRTT: ",RTTend)
 print("Recibiendo tiempo de servidor ",tiempoServidor)
-
-
+        
+        
 """
-Convirtiendo el mensaje decodificado a su respectivo formato datetime para su manipulación
-"""
+        Convirtiendo el mensaje decodificado a su respectivo formato datetime para su manipulación
+""" 
 tiempoS=datetime.datetime.strptime(tiempoServidor, "%H:%M:%S")
-
+        
 """
-Calculando el nuevo tiempo del proceso P
+        Calculando el nuevo tiempo del proceso P
 """
-tiempoP=tiempoS-(datetime.timedelta(seconds=(int(RTT/2))))
+tiempoP=tiempoS-(datetime.timedelta(seconds=(int(RTTend/2))))
 print ("Nueva hora del proceso P\n\t", tiempoP.strftime("%H:%M:%S"))
-
-
-
-
+print("""    
+                            REPORTE DE SINCRONIZACIÓN
+            ______________________________________________________________________________________________
+            |  Tiempo original   | Comienzo de solicitud    | Promedio Trans-Rec   |     Hora ajustada    |
+            |    del cliente     |     (System Clocks)      |        (RTT)         |                      |
+            |----------------------------------------------------------------------|----------------------
+            """)
+print("\t\t",tiempoPold.strftime("%H:%M:%S"),"\t     ",round(RTTstart),"\t\t ",RTTend,"\t",tiempoP.strftime("%H:%M:%S"))
+     
--- Servidor.py
+++ Servidor.py
@@ -14,7 +14,7 @@ Conexión ZMQ
 """
 context=zmq.Context() 
 socket=context.socket(zmq.REP)
-string=socket.bind("tcp://*:4595")
+string=socket.bind("tcp://*:6865")
 
 
 """
@@ -24,19 +24,22 @@ while True:
     print ("En espera de solicitud de tiempo....")
     solicitudP=socket.recv()
     print (solicitudP.decode('utf-8'))
-    print("Solicitud de tiempo recibida satisfactoriamente")    
-    """
-    Convirtiendo fecha a string para su respectiva envío
-    """
-    tiempoS=datetime.datetime.now().strftime("%H:%M:%S")
-    print("Enviando respuesta a P\n\t Hora del servidor:\t",tiempoS)
-    print("Simulando latencia de envío")
-    for x in range(5):
-        print(".")
-        time.sleep(random.randint(0,1)) #Simulando latencia de respuesta    
-
+    print("Solicitud recibida satisfactoriamente")
+    if solicitudP.decode('utf-8')=="solicitud de tiempo":
+        """
+        Convirtiendo fecha a string para su respectiva envío
+        """
+        tiempoS=datetime.datetime.now().strftime("%H:%M:%S")
+        print("Enviando respuesta a P\n\t Hora del servidor:\t",tiempoS)
+        print("Simulando latencia de envío")
+        for x in range(3):
+            print(".")
+            time.sleep(random.randint(0,1)) #Simulando latencia de respuesta    
     
-    socket.send(tiempoS.encode('utf-8'))
-    print("Tiempo enviado satisfactoriamente")
-    print("En espera de la siguiente solicitud..."+"\n"*3)
-
+        
+        socket.send(tiempoS.encode('utf-8'))
+        print("Tiempo enviado satisfactoriamente")
+        print("En espera de la siguiente solicitud..."+"\n"*3)
+    else:
+       pass
+    
