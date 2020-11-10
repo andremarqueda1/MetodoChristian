938956472caa6021f1fb6c5d387251c55e8da3dd
andremarqueda <andremarqueda1@gmail.com>
2020-11-09 21:10:04 -0600

    Sincronización 60% realizada,Falta calcular RTT

--- Cliente.py
+++ Cliente.py
@@ -6,20 +6,29 @@ Implementación a través de ZeroMQ
 """
 
 import zmq
-
+import datetime
+import random
 context = zmq.Context()
-from datetime import datetime
-tiempoP=datetime.now()
-#  Socket to talk to server
+
+tiempoP=datetime.datetime.now()#Hora de P
+"""
+Restando segundos aleatorios para simular una desincronización
+"""
+tiempoP=tiempoP-datetime.timedelta(seconds=random.randint(0,9))
+print("La hora actual en P previo a la sincronización es:\n",tiempoP)
 print("Conectandose al proceso S…")
+
 socket = context.socket(zmq.REQ)
 socket.connect("tcp://localhost:4595")
-
-
 print("Solicitando tiempo a S")
 msg = "solicitud de tiempo"
 socket.send(msg.encode('utf-8'))
 
-    #  Get the reply.
-message = socket.recv()
+"""
+Recibiendo respuesta del servidor S
+"""
+message = socket.recv() #Recibiendo tiempo
 print("Recibiendo tiempo y realizando ajuste ",message.decode('utf-8'))
+"""
+Se debe calcular el RTT 
+"""
--- Servidor.py
+++ Servidor.py
@@ -4,21 +4,23 @@ Python 3.7.4 (default, Aug  9 2019, 18:34:13) [MSC v.1915 64 bit (AMD64)]
 Método de Christian para la sincronización de relojes
 Implementación a través de ZeroMQ
 """
-from datetime import datetime
 import zmq
-import random
+import datetime
+
+
 
 context=zmq.Context() 
 socket=context.socket(zmq.REP)
 string=socket.bind("tcp://*:4595")
 
 
-while True:
-    solicitudP=socket.recv()
-    print (solicitudP.decode('utf-8'))
-    print("Solicitud de tiempo recibida satisfactoriamente")
-    
-    tiempoS=datetime.now().strftime("%H:%M:%S")
-    socket.send(tiempoS.encode('utf-8'))
+print ("En espera de solicitud de tiempo....")
+solicitudP=socket.recv()
+print (solicitudP.decode('utf-8'))
+print("Solicitud de tiempo recibida satisfactoriamente")    
+tiempoS=datetime.datetime.now().strftime("%H:%M:%S")
+print("Enviando respuesta a P\n\t Hora del servidor:\t",tiempoS)
+socket.send(tiempoS.encode('utf-8'))
+print("Tiempo enviado satisfactoriamente")
     
 
